db2f9b937f59223d8618fcbdde026979faa26b6f
Sebastian Kienitz <sebastian.kienitz@gmx.de>
2018-02-18 11:44:51 -1200

    Update scan removeable storage and sync.py

--- scan removeable storage and sync.py
+++ scan removeable storage and sync.py
@@ -1,36 +1,35 @@
-import os, string, win32api, time, sys
+import os, string, win32api,time, sys
 from dirsync import sync
 import ctypes
+import threading
 
-# LOGGING-----------------------------------
-te = open('MicroSD_Readout_log_' + time.strftime("%Y_%m_%d_%Hh_%Mm") + '.txt',
-          'w')  # File where you need to keep the logs
-
+#LOGGING-----------------------------------
+te = open('MicroSD_Readout_log_' + time.strftime("%Y_%m_%d_%Hh_%Mm") + '.txt','w')  # File where you need to keep the logs
 
 class Unbuffered:
-    def __init__(self, stream):
-        self.stream = stream
-
-    def write(self, data):
-        self.stream.write(data)
-        self.stream.flush()
-        te.write(data)  # Write the data of stdout here to a text file as well
-
-
-sys.stdout = Unbuffered(sys.stdout)
+   def __init__(self, stream):
+       self.stream = stream
+   def write(self, data):
+       self.stream.write(data)
+       self.stream.flush()
+       te.write(data)    # Write the data of stdout here to a text file as well
+sys.stdout=Unbuffered(sys.stdout)
 
 
-# FUNCTIONS---------------------------------
+#FUNCTIONS & CLASSES---------------------------------
+
+# Define a function for the thread
+def SD_copyjob(sourcedir, targetdir):
+   sync(sourcedir, targetdir, 'sync', verbose=True, modtime=True)
+
 def Mbox(title, text, style):
     return ctypes.windll.user32.MessageBoxA(0, text, title, style)
-
 
 def get_used_drive_letters():
     drives = win32api.GetLogicalDriveStrings()
     drives = drives.split('\000')[:-1]
     letters = [d[0] for d in drives]
     return letters
-
 
 def flushdir(dir, time_offset_sec):
     now = time.time()
@@ -40,82 +39,80 @@ def flushdir(dir, time_offset_sec):
             if os.path.isfile(fullpath):
                 os.remove(fullpath)
             elif os.path.isdir(fullpath):
-                flushdir(fullpath, time_offset_sec)
+                flushdir(fullpath,time_offset_sec)
 
 
-# -----------VARIABLES---------------------
-target_directory_name = '/Blackvue'
-source_directory_name = 'BlackVue/Record'
+#-----------VARIABLES---------------------           
+target_directory_name=''
+source_directory_name=''
 root_dir = os.getcwd()
-msg_temp = ''
-flush_delay = 2419200
-# -----------------------------------------MAIN-------------------------------------------------------------------------
+msg_temp=''
+flush_delay=2419200
+SD_CARD=[]
+threads = []
+#-----------------------------------------MAIN-------------------------------------------------------------------------
 
 print 'STATION-H4: looking for connected SD-Cards............. \n'
 
-while 1 == 1:
+while 1==1:
     """print 'search for directories: '"""
-    directories = []
+    directories=[]
     for f in os.listdir(root_dir):
         if os.path.isfile(os.path.join(root_dir, f)):
-            1 == 1
+            1==1
         else:
             directories.append(f)
 
     """print 'search for drives: '"""
-    drives = []
-    labels = []
-    inserted_drives = get_used_drive_letters()
+    drives= []
+    labels=[]
+    inserted_drives=get_used_drive_letters()
     for i in inserted_drives:
-        i = ''.join(map(str, i)) + ':/'
+        i=''.join(map(str, i))+':/'
         try:
-            label = win32api.GetVolumeInformation(i)[0]
+            label=win32api.GetVolumeInformation(i)[0]
         except:
-            label = 'Not_Available'
+            label='Not_Available'
         labels.append(label)
         drives.append(i)
 
-    index = [i for i, item in enumerate(labels) if item in directories]
-
+    index=[i for i, item in enumerate(labels) if item in directories]
+        
     if not index:
-        print '------------No MicroSD found------------------'
-        Mbox('STATION-H4',
-             'There wasnt any SD-Card matching to a AU# found:\n\nA)Please re-instert them into the USB and check if they appear in the File-Explorer\n\nB)Make sure there is a Folder created with the AU# on the Blackvue-Harddrive ' + root_dir + '\n\nC)Make sure the SD-Card is labeled (File-Explorer) with the AU#',
-             1)
+            print '------------No MicroSD found------------------'
+            Mbox('STATION-H4', 'There wasnt any SD-Card matching to a AU# found:\n\nA)Please re-instert them into the USB and check if they appear in the File-Explorer\n\nB)Make sure there is a Folder created with the AU# on the Blackvue-Harddrive '+ root_dir + '\n\nC)Make sure the SD-Card is labeled (File-Explorer) with the AU#', 1)
     else:
         for i in index:
-            drive_letter = ''.join(map(str, drives[i]))
-            label = ''.join(map(str, labels[i]))
-            print '----------------------Micro SD found on ' + drive_letter + '  labeled as ' + label + '\n'
+                drive_letter=''.join(map(str, drives[i]))
+                label=''.join(map(str, labels[i]))
+                print '----------------------Micro SD found on ' + drive_letter + '  labeled as ' + label + '\n'
 
         for i in index:
-            drive_letter = ''.join(map(str, drives[i]))
-            label = ''.join(map(str, labels[i]))
-            msg_temp = msg_temp + label + '\n'
-        Mbox('STATION-H4',
-             'Following SD-Cards were found:\n\n' + msg_temp + '\nIs that correct? (If not hit cancel and make sure all SD-Cards were detected in the File-Explorer and restart program)',
-             1)
+                drive_letter=''.join(map(str, drives[i]))
+                label=''.join(map(str, labels[i]))
+                msg_temp = msg_temp + label + '\n'
+        Mbox('STATION-H4','Following SD-Cards were found:\n\n'+msg_temp+'\nIs that correct? (If not hit cancel and make sure all SD-Cards were detected in the File-Explorer and restart program)', 1)
 
         for i in index:
-            drive_letter = ''.join(map(str, drives[i]))
-            label = ''.join(map(str, labels[i]))
-            targetdir = root_dir + '/' + label + target_directory_name
-            sourcedir = drive_letter + source_directory_name
-            if not os.path.isdir(targetdir):
-                os.makedirs(targetdir)
-            print 'copying from: ' + sourcedir
-            if not os.path.isdir(sourcedir):
-                Mbox('STATION-H4',
-                     'NO SOURCE DIRECTORY FOUND - Make sure SD-Card contains a Record Folder that contains data: ' + sourcedir,
-                     1)
-            print 'copying to: ' + targetdir
-            sync(sourcedir, targetdir, 'sync', verbose=True, modtime=True)
-            print '---------------REMOVING OLD DATA in: ' + targetdir + ' --------------- \n'
-            flushdir(targetdir, flush_delay)
+                drive_letter=''.join(map(str, drives[i]))
+                label=''.join(map(str, labels[i]))
+                targetdir =root_dir + '/' + label + target_directory_name
+                sourcedir = drive_letter + source_directory_name
+                if not os.path.isdir(targetdir):
+                   os.makedirs(targetdir)
+                print 'copying from: ' + sourcedir
+                if not os.path.isdir(sourcedir):
+                   Mbox('STATION-H4','NO SOURCE DIRECTORY FOUND - Make sure SD-Card contains a Record Folder that contains data: ' + sourcedir, 1)
+                print 'copying to: ' + targetdir
+                t = threading.Thread(target=SD_copyjob,args=(sourcedir, targetdir))
+                """threads.append(t)"""
+                t.start()
 
-    print 'DONE - REMOVE CARDS \n'
-
-    input("Press enter to close program")
-    time.sleep(2)
+    """t.start()"""
+    """t.join()"""
+    """print 'DONE - REMOVE CARDS \n'
+    
+    input("Press enter to close program")"""
     break
-
+ 
+    
